d2b767664a34021d9714b50d50972d382ca0aec6
RT <me@rtflores.com>
2025-01-13 17:10:55 -0800

    implement initializable test file for network_node

---
+++ tests/test_network_node.py
@@ -0,0 +1,41 @@
+import sys, os
+sys.path.append(os.getcwd()[0:os.getcwd().find('/GSE') + 4] + '/launch_controller')
+from network_node import SendNode, ReceiveNode
+from telem_codec import TelemCodec
+from command_codec import CommandCodec
+
+TLM_SERVER_ADDR_IP = ""
+TLM_SERVER_ADDR_PORT = 31000
+CMD_RECEIVER_ADDR_IP = ""
+CMD_RECEIVER_ADDR_PORT = 31002
+GC_ADDR_IP = "10.0.0.100"
+GC_ADDR_PORT = 31000
+
+class TestNetworkNode:
+	def setup_method(self, method):
+		print(f"Setting up {method}")
+
+		self.send_node = SendNode((TLM_SERVER_ADDR_IP, TLM_SERVER_ADDR_PORT), (GC_ADDR_IP, GC_ADDR_PORT), TelemCodec())
+		self.recieve_node = ReceiveNode((CMD_RECEIVER_ADDR_IP, CMD_RECEIVER_ADDR_PORT), CommandCodec())
+
+	def teardown_method(self, method):
+		print(f"Tearing down {method}")
+
+
+# TESTING __INIT__()
+	def test_initialization(self):
+		assert True
+
+# TESTING SEND_NODE.ENCODE(SELF, MSG)
+
+
+
+# TESTING SEND_NODE.DECODE(SELF, PACKET)
+
+
+
+# TESTING RECEIVE_NODE.ENCODE(SELF, MSG)
+
+
+
+# TESTING RECEIVE_NODE.DECODE(SELF, PACKET)
