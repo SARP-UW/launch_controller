210abeee19ff925f51fce7542bfe33a834f87b3b
RT <me@rtflores.com>
2025-01-13 16:49:05 -0800

    implement initializable test file for controller

---
+++ tests/test_controller.py
@@ -0,0 +1,17 @@
+import sys, os
+sys.path.append(os.getcwd()[0:os.getcwd().find('/GSE') + 4] + '/launch_controller')
+from controller import Controller
+
+class TestController:
+	def setup_method(self, method):
+		print(f"Setting up {method}")
+        
+		self.relays = Controller()
+
+	def teardown_method(self, method):
+		print(f"Tearing down {method}")
+
+
+# TESTING __INIT__()
+	def test_initialization(self):
+		assert True
