cd5f0bcda125b9778e43eb6a476e49360817dfdb
RT <me@rtflores.com>
2025-01-26 23:08:58 -0800

    add troubleshooting message

--- venv.sh
+++ venv.sh
@@ -3,6 +3,8 @@ if [ -e .venv/bin/activate ]; then
 else
     echo "[ Creating virtual environment ]"
 	echo ""
+	echo "NOTE: Restart the terminal session and run this script again or the dependencies will not be recognized by python."
+	echo ""
 	python3 -m venv .venv
 	source .venv/bin/activate
 fi
