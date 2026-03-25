b98ea7d89dbf2196883501c18c5d4deb6f520531
Aaron McBride <151467777+aaron-mcbride@users.noreply.github.com>
2026-03-24 21:13:29 -0700

    Mock mode false

--- src/settings.py
+++ src/settings.py
@@ -13,7 +13,7 @@ WEBSITE_HEARTBEAT_TIMEOUT: float = 3.0
 ###################################################################################################
 
 # If true, application runs in "mock" mode (simulates hardware interaction)
-MOCK_MODE: bool = True
+MOCK_MODE: bool = False
 
 # If true, application prints valve state changes to console
 PRINT_VALVE_STATES: bool = True
